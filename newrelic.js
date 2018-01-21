97028066a5c333fa35a3d7275783b779255fdd9d
Adjectival <alexd.jacks@gmail.com>
2018-01-21 12:52:54 -0800

    cleans up party zone & makes name size dynamic to list vs party

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config/default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Fantasy War Tactics Helper'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'a6bca7415cbcbab8370391d355c32c05d9a8fdb9',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
